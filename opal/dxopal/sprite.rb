52eb26c6cb84f95088b75f85c1fe1e5e3440d63c
Yutaka HARA <yutaka.hara+github@gmail.com>
2017-04-23 15:56:13 +0900

    new: Sprite.draw

--- opal/dxopal/sprite.rb
+++ opal/dxopal/sprite.rb
@@ -1,11 +1,22 @@
 module DXOpal
   class Sprite
+    def self.draw(sprites)
+      sprites.flatten.sort_by(&:z).each do |sprite|
+        next if sprite.respond_to?(:vanished?) && sprite.vanished?
+        sprite.draw
+      end
+    end
+
     def initialize(x=0, y=0, image=nil)
       @x, @y, @image = x, y, image
       @z = 0
       @visible = true
+      @vanished = false
     end
     attr_accessor :x, :y, :z, :image, :visible
+
+    def vanish; @vanished = true; end
+    def vanished?; @vanished; end
 
     def draw
       raise "image not set to Sprite" if @image.nil?
